4539a9ded4be4880e4acdca6e035ffd9665d0edf
RaihanFeb <raihanfebriahdi@gmail.com>
2024-10-07 21:24:36 +0700

    Update UTS no 4.cpp

--- UTS no 4.cpp
+++ UTS no 4.cpp
@@ -1,64 +1,26 @@
 #include <iostream>
 using namespace std;
 
-// Fungsi untuk operasi penjumlahan
-double tambah(double x, double y) {
-  return x + y;
-}
-
-// Fungsi untuk operasi pengurangan
-double kurang(double x, double y) {
-  return x - y;
-}
-
-// Fungsi untuk operasi perkalian
-double kali(double x, double y) {
-  return x * y;
-}
-
-// Fungsi untuk operasi pembagian
-double bagi(double x, double y) {
-  if (y == 0) {
-    cout << "Pembagian dengan nol tidak terdefinisi" << endl;
-    return 0; // Mengembalikan 0 jika terjadi pembagian dengan nol
-  } else {
-    return x / y;
-  }
-}
-
 int main() {
-  int pilihan;
-  double angka1, angka2;
+    int batas;
 
-  cout << "Pilih operasi:" << endl;
-  cout << "1. Tambah" << endl;
-  cout << "2. Kurang" << endl;
-  cout << "3. Kali" << endl;
-  cout << "4. Bagi" << endl;
-  cout << "Masukkan pilihan (1/2/3/4): ";
-  cin >> pilihan;
 
-  cout << "Masukkan angka pertama: ";
-  cin >> angka1;
-  cout << "Masukkan angka kedua: ";
-  cin >> angka2;
+    cout<<"Masukan batas : ";
+    cin>>batas;
+    cout<<endl;
+    cout<<"Deret bilangan ganjil"<<endl;
 
-  switch (pilihan) {
-    case 1:
-      cout << "Hasil dari penjumlahan untuk " << angka1 << " dan " << angka2 << " adalah " << tambah(angka1, angka2) << endl;
-      break;
-    case 2:
-      cout << "Hasil dari pengurangan untuk " << angka1 << " dan " << angka2 << " adalah " << kurang(angka1, angka2) << endl;
-      break;
-    case 3:
-      cout << "Hasil dari perkalian untuk " << angka1 << " dan " << angka2 << " adalah " << kali(angka1, angka2) << endl;
-      break;
-    case 4:
-      cout << "Hasil dari pembagian untuk " << angka1 << " dan " << angka2 << " adalah " << bagi(angka1, angka2) << endl;
-      break;
-    default:
-      cout << "Input tidak valid" << endl;
-  }
+    for (int i=1;i<=batas;i++) {
+        if(i%2!=0) {
+            cout<<i<<" ";
+        }
+    }
+    cout<<"Deret bilangan genap"<<endl;
 
-  return 0;
+    for (int i=1;i<=batas;i++) {
+        if(i%2==0) {
+            cout<<i<<" ";
+        }
+     }
+    return 0;
 }
