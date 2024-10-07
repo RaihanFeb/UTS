032af38da90f0794bdc535971e7d177a51801f3b
RaihanFeb <raihanfebriahdi@gmail.com>
2024-10-07 21:26:41 +0700

    Update UTS no 3.cpp

--- UTS no 3.cpp
+++ UTS no 3.cpp
@@ -1,30 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// Fungsi untuk operasi penjumlahan
-double tambah(double x, double y) {
-  return x + y;
-}
-
-// Fungsi untuk operasi pengurangan
-double kurang(double x, double y) {
-  return x - y;
-}
-
-// Fungsi untuk operasi perkalian
-double kali(double x, double y) {
-  return x * y;
-}
-
-// Fungsi untuk operasi pembagian
-double bagi(double x, double y) {
-  if (y == 0) {
-    cout << "Pembagian dengan nol tidak terdefinisi" << endl;
-    return 0; // Mengembalikan 0 jika terjadi pembagian dengan nol
-  } else {
-    return x / y;
-  }
-}
 
 int main() {
   int pilihan;
